adafe7b2115775c9120891c180ed3fb4404d4f2b
dkeari <doug.keari@gmail.com>
2021-05-16 21:19:16 +1000

    End of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using API.Extensions;
 
 namespace API
 {
@@ -30,17 +21,12 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddDbContext<DataContext>(options =>
-      {
-        options.UseSqlite(__config.GetConnectionString("DefaultConnection"));
-      });
-
+     
+     services.AddApplicationServices(__config);
      services.AddControllers();
      services.AddCors();
-     //services.AddSwaggerGen(c =>
-    // {
-     //  c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-    // });
+     services.AddIdentityServices(__config);
+
      }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,8 +35,6 @@ namespace API
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
-      //  app.UseSwagger();
-       // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
       }
 
       app.UseHttpsRedirection();
@@ -58,6 +42,8 @@ namespace API
       app.UseRouting();
 
       app.UseCors(x => x.WithOrigins("https://localhost:4200").AllowAnyMethod().AllowAnyHeader());
+
+      app.UseAuthentication();
 
       app.UseAuthorization();
 
