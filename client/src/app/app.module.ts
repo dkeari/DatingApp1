6ec72c70796cd9ccb53ca2d9b1e8a8922f3d9465
dkeari <doug.keari@gmail.com>
2021-06-06 04:37:52 +1000

    End of section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,12 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule} from 'ngx-toastr';
+
 
 
 @NgModule({
@@ -17,7 +23,11 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +35,9 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     FormsModule,
     BsDropdownModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass:'toast-bottom-right'})
   ],
   providers: [],
   bootstrap: [AppComponent]
